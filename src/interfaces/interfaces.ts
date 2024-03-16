1807e09174eba6ee1d4dbf8fb19b0c77424c54d6
Attila Reterics <reterics.attila@gmail.com>
2024-03-16 14:30:06 +0100

    Initial Firebase + Vite structure

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,22 @@
+import { User } from 'firebase/auth'; //type User import
+
+
+export interface LoginFormValues {
+    email: string;
+    password: string;
+}
+
+export interface UserFormValues {
+    email: string;
+    password: string;
+    displayName: string;
+}
+
+//IAuth context
+export  interface  IAuth {
+    user:  User  |  null;  //type User comes from firebase
+    loading:  boolean;
+    SignIn: (credentials:  LoginFormValues) =>  void;
+    SignUp: (credentials:  UserFormValues) =>  void;
+    SignOut: () =>  void;
+}
---
+++ src/store/AuthContext.tsx
@@ -0,0 +1,113 @@
+import {firebaseAuth} from '../firebase/BaseConfig';
+import React, {createContext, useEffect, useState} from 'react';
+import {IAuth, LoginFormValues, UserFormValues} from "../interfaces/interfaces.ts";
+import {useNavigate} from 'react-router-dom';
+import {SignIn, SignOut, SignUp} from "../firebase/services/AuthService.ts";
+import {onAuthStateChanged, User} from 'firebase/auth';
+import PageLoading from "../components/PageLoading.tsx";
+
+export const AuthContext = createContext<IAuth>({
+    user: firebaseAuth.currentUser,
+    loading: false,
+    SignIn: () => {},
+    SignUp: () => {},
+    SignOut: () => {},
+});
+
+
+const  AuthProvider  = ({ children }:  {children: React.ReactNode}) => {
+
+    const [currentUser,  setCurrentUser] =  useState<User  |  null>(null);
+    const [isLoading,  setIsLoading] =  useState<boolean>(false);
+    const [isAuthLoading,  setIsAuthLoading] =  useState<boolean>(true);
+    const  navigate  =  useNavigate();
+
+
+    const SignUpMethod = (creds:  UserFormValues) => {
+        setIsLoading(true);
+        SignUp(creds)
+            .then(async userCredential  => {
+                const { user } = userCredential; //object destructuring
+                if (user) {
+                    setCurrentUser(user);
+                    //redirect the user on the targeted route
+                    navigate('/dashboard', { replace:  true });
+                } else {
+                    //TODO: Handle if user is empty
+                }
+                setIsLoading(false);
+            })
+            .catch(error  => {
+                //check for error
+                if (error.code  ===  'auth/email-already-in-use') {
+                    //show an alert or console
+                } else if (error.code  ===  'auth/too-many-requests') {
+                    //do something like an alert
+                }
+                // you can check for more error like email not valid or something
+                setIsLoading(false);
+            });
+    }
+
+    const SignInMethod = async (creds:  LoginFormValues) => {
+        setIsLoading(true);
+        SignIn(creds)
+            .then(userCredential  => {
+                const { user } =  userCredential;
+                if  (user) {
+                    setCurrentUser(user);
+                    //redirect user to targeted route
+                    navigate('/dashboard', { replace:  true });
+                }
+                else {
+                    // TODO: Handle if user is empty
+                }
+                setIsLoading(false);
+                })
+            .catch(error  => {
+                    if  (error.code  ===  'auth/wrong-password') {
+                        //TODO: show error
+                    } else  if  (error.code  ===  'auth/too-many-requests') {
+                        //TODO: show error
+                    }
+                    setIsLoading(false);
+                });
+    }
+
+    const SignOutMethod = async () => {
+        setIsLoading(true);
+        try {
+            await SignOut();
+            setCurrentUser(null);
+            navigate('/signin', { replace:  true });
+        } catch  (error) {
+            setIsLoading(false);
+            //show error alert
+        }
+    }
+    //create Auth Values
+    const authValues: IAuth = {
+        user: currentUser,
+        loading: isLoading,
+        SignIn: SignInMethod,
+        SignUp: SignUpMethod,
+        SignOut: SignOutMethod,
+    }
+
+    useEffect(() => {
+        //onAuthStateChanged check if the user is still logged in or not
+        return  onAuthStateChanged(firebaseAuth, user => {
+            setCurrentUser(user);
+            setIsAuthLoading(false);
+        });
+    },  []);
+
+    //If loading for the first time when visiting the page
+    if (isAuthLoading) return <PageLoading />;
+
+    return (
+        <AuthContext.Provider value={authValues}>{children}</AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
